75ffcd59abbc0530fdc1b8dc8f896065cf080e4d
fedelondon <felova21@gmail.com>
2022-09-28 13:59:55 -0500

    re-structure proyect

---
+++ backend/src/routes/users.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+user = APIRouter()
+
+@user.get('/users/{name}')
+async def user_get(name: str):
+    return {f"Hello, {name}"}
+
+@user.get('/users/{name}/{salary}')
+async def user_salary(name: str, salary: float):
+    return {f"Hello {name} your salary is {salary}"}
--- routes/users.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-user = APIRouter()
-
-@user.get('/users')
-def user_get():
-    return "Hello"
